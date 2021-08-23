939e0145d6a25e02909b7bd86a8958f922f270ac
338317 <zg338317@gmail.com>
2021-08-24 00:32:32 +0800

    修改了配置文件加载逻辑，并使用指针以减少资源损耗

--- main.go
+++ main.go
@@ -16,30 +16,21 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/driver"
 )
 
+var config *utils.Config
+
 func init() {
-
 	utils.InitLogger()
-
 	log.Info("项目地址：https://github.com/Kyomotoi/go-ATRI")
-	log.Info("当前版本："+service.Version())
+	log.Info("当前版本：" + service.Version())
 	log.Info("后宫裙：567297659")
-
-	err := utils.InitConfig()
-	if err != nil {
-		log.Warning("初始化 config.yml 失败")
-		log.Warning("将于5秒后退出")
-		time.Sleep(time.Second * 5)
-		os.Exit(1)
-	}
-
-	conf, err := utils.ConfigDealer()
+	var conf, err = utils.ConfigDealer()
 	if err != nil {
 		log.Warning("处理 config.yml 失败")
 		log.Warning("将于5秒后退出")
 		time.Sleep(time.Second * 5)
 		os.Exit(1)
 	}
-
+	config = conf
 	if conf.Debug {
 		log.Info("DEBUG 已启用")
 		log.SetLevel(log.DebugLevel)
@@ -50,22 +41,21 @@ func init() {
 	if terminal.RunningByDoubleClick() {
 		log.Warning("不建议直接双击运行本程序，这将导致一些非可预料后果，请通过控制台启动本程序")
 		log.Warning("将等待10秒后启动")
-		time.Sleep(time.Second*10)
+		time.Sleep(time.Second * 10)
 	}
 
 	log.Info("アトリは、高性能ですから！")
 }
 
 func main() {
-	conf, _ := utils.ConfigDealer()
 	zero.Run(zero.Config{
-		NickName:      conf.Nickname,
-		CommandPrefix: conf.CommandPrefix,
-		SuperUsers:    conf.SuperUsers,
-		Driver:        []zero.Driver{
+		NickName:      config.Nickname,
+		CommandPrefix: config.CommandPrefix,
+		SuperUsers:    config.SuperUsers,
+		Driver: []zero.Driver{
 			&driver.WSClient{
-				Url:         conf.WebsocketURL,
-				AccessToken: conf.AccessToken,
+				Url:         config.WebsocketURL,
+				AccessToken: config.AccessToken,
 			},
 		},
 	})
